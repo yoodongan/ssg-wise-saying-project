d2ba646e78118f871a47ddcb109134dd8b958510
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-07-16 15:59:13 +0900

    객체 저장 시 , id 자동 증가하는 기능 구현

--- src/main/java/com/ll/proverb/WiseSayingTable.java
+++ src/main/java/com/ll/proverb/WiseSayingTable.java
@@ -11,4 +11,23 @@ public class WiseSayingTable {
         String body = "내용";
         Util.file.saveToFile("%s/wise_saying/%d.json".formatted(baseDir, wiseSaying.getId()), body);
     }
+    public void save(String content, String author) {
+        int id = getLastId() + 1;  // save 할 때마다, id 1 증가.
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        save(wiseSaying);
+        saveId(id);
+
+    }
+
+    private void saveId(int id) {
+        Util.file.saveToFile("%s/wise_saying/saveId.txt".formatted(baseDir), id + "");
+    }
+
+    private int getLastId() {
+        String id = Util.file.readFromFile("%s/wise_saying/saveId.txt".formatted(baseDir), "");
+        if (id.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(id);
+    }
 }
--- src/test/java/WiseSayingTableTest.java
+++ src/test/java/WiseSayingTableTest.java
@@ -10,9 +10,18 @@ public class WiseSayingTableTest {
     @Test
     public void 등록을_하면_명언과_작가를_물어본다() {
         WiseSayingTable wiseSayingTable = new WiseSayingTable("test_data");
-        WiseSaying wiseSaying = new WiseSaying(1, "나에게 불가능이란 없다.", "나폴레옹");
+        WiseSaying wiseSaying = new WiseSaying(1, "진짜 문제는 사람들의 마음이다. 그것은 절대로 물리학이나 윤리학의 문제가 아니다", "아인슈타인");
         wiseSayingTable.save(wiseSaying);
 
         assertTrue(new File("test_data/wise_saying/1.json").exists());
+    }
+    @Test
+    public void 저장() {  // 객체 저장 시,  id 자동 증가하는 기능 테스트.
+        WiseSayingTable wiseSayingTable = new WiseSayingTable("test_data");
+
+        wiseSayingTable.save("진짜 문제는 사람들의 마음이다. 그것은 절대로 물리학이나 윤리학의 문제가 아니다", "아인슈타인");
+        assertTrue(new File("test_data/wise_saying/1.json").exists());
+        wiseSayingTable.save("나는 생각한다. 고로 나는 존재한다.", "데카르트");
+        assertTrue(new File("test_data/wise_saying/2.json").exists());
     }
 }
